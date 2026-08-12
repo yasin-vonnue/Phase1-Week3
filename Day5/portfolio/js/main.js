82b44d2b1d0066bdb21513f2f31044a4d4de6ad1
yasin-vonnue <muhammad.yasin@vonnue.com>
2026-08-12 18:09:54 +0530

    feat: wired all core features to the 6 pages

---
+++ Day5/portfolio/js/main.js
@@ -0,0 +1,15 @@
+import { setupNavigation } from "./components/nav.js";
+import { setupDarkMode } from "./components/darkMode.js";
+import { setupAccordion } from "./components/accordion.js";
+import { setupLightbox } from "./components/lightbox.js";
+import { setupScrollAnimations } from "./components/scrollAnimations.js";
+import { setupBackToTop } from "./components/backToTop.js";
+import { setupReadingProgress } from "./components/readingProgress.js";
+
+setupNavigation();
+setupDarkMode();
+setupAccordion();
+setupLightbox();
+setupScrollAnimations();
+setupBackToTop();
+setupReadingProgress();
